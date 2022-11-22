db8b9ac8b8c1e0c4b0afcc6371157c04c58f1889
SardorUsmon <sardorusmon111@gmail.com>
2022-11-23 01:09:18 +0500

    Initial Commit

---
+++ 99 name of allah/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  99 name of allah
+//
+//  Created by Sardor Panjiyev on 23/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
